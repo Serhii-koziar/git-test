1c2d4f9a3453a18d76cf9d75ee252e4b40f44410
Serhii Koziar <catalonya1111@gmail.com>
2020-11-23 19:39:28 +0200

    console

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 document.body.innerHtml  = '    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, dignissimos doloremque est harum illo ipsa natus perspiciatis quaerat! At consectetur deleniti eos magni quia quo tempora unde voluptatem. Repellendus, veniam.</h1>';
+console.log('testgit');
