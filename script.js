68732187e126b842536ddf3eba741ab72e9ca7b3
Serhii Koziar <catalonya1111@gmail.com>
2020-11-23 19:04:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,1 @@
+document.body.innerHtml  = '    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, dignissimos doloremque est harum illo ipsa natus perspiciatis quaerat! At consectetur deleniti eos magni quia quo tempora unde voluptatem. Repellendus, veniam.</h1>';
